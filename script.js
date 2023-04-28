721af180a490a1dc4f6feb3cc61a963682fb16b9
Maria <MariaMDeeva@yandex.ru>
2023-04-28 17:17:27 +0300

    Начата работа с карточкой, сохранение имени в профиле

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ let popupContainer = document.querySelector('.popup__container');
 
 let formPopup = document.querySelector('.popup__form');
 let namePopup = document.querySelector('#popup__name');
-let aboutMeNamePopup = document.querySelector('#popup__aboutme');
+let aboutMePopup = document.querySelector('#popup__aboutme');
 let nameProfil = document.querySelector('.profil__fullname');
-let aboutMeNameProfil = document.querySelector('.profil__aboutme');
+let aboutMeProfil = document.querySelector('.profil__aboutme');
 
 let popupNewCard = document.querySelector('#popupCard');
 let addCardButton = document.querySelector('.profil__add');
@@ -17,6 +17,8 @@ let closeCardButton = document.querySelector('.popup__close-button_card');
 // Открытие-закрытие попапа редактирования профиля
 editProfilButton.addEventListener('click', function(){
     popupProfil.classList.add('popup_opened');
+    namePopup.value = nameProfil.textContent;
+    aboutMePopup.value = aboutMeProfil.textContent;
 })
 closeProfilButton.addEventListener('click',function(){
     popupProfil.classList.remove('popup_opened');
@@ -26,7 +28,7 @@ closeProfilButton.addEventListener('click',function(){
 formPopup.addEventListener('submit', function addName(evt){
     evt.preventDefault();
     nameProfil.textContent = namePopup.value;
-    aboutMeNameProfil.textContent = aboutMeNamePopup.value
+    aboutMeProfil.textContent = aboutMePopup.value
     popupProfil.classList.remove('popup_opened');
 });
 
@@ -37,3 +39,29 @@ addCardButton.addEventListener('click', function(){
 closeCardButton.addEventListener('click', function(){
     popupNewCard.classList.remove('popup_opened');
 });
+    const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
