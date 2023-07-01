54a0300143ffdc3164c99a954823c128e40ff414
Kirill Romanov <romanov-km@MacBook-Air-Kirill.local>
2023-07-01 11:58:35 +0300

    fix: удалили случайную ошибку

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ Promise.all([getUserInfo(), getCards()])
   .catch((err) => console.log(`Ошибка: ${err}`));
 
 
-git addCardButton.addEventListener('click', () => {
+addCardButton.addEventListener('click', () => {
   openPopup(newCardPopup);
   
   toggleButtonState(newCardFormPopup, validConfig);
